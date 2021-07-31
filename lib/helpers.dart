8810acbbea9245bf4579fb18dfec2e7bf126b68e
Saleh Alzahrani <saleh_ahmed@outlook.com>
2021-07-31 22:35:33 +0300

    Update helpers.dart

--- lib/helpers.dart
+++ lib/helpers.dart
@@ -3,7 +3,7 @@ import 'package:path_provider/path_provider.dart';
 class Helpers {
   static String? path;
 
-  Future<String> getAppDocumentsDirectoryPath({String? filename}) async {
+  static Future<String> getAppDocumentsDirectoryPath({String? filename}) async {
     if (path == null) {
       path = (await getApplicationDocumentsDirectory()).path;
       print(path);
